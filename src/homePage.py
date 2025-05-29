80ada0e67c5135207ac5ac3755d7a6ce5ad7a5ec
DonLuisM <lfmb03@outlook.com>
2025-05-28 20:45:18 -0500

    feat(Updates): Update Model Interface RNN ST

--- src/homePage.py
+++ src/homePage.py
@@ -4,6 +4,32 @@ from tensorflow import keras
 from tensorflow.keras.layers import TextVectorization
 import numpy as np
 
+
+# * Configuración de funciones
+def cargar_modelo():
+    modelo = keras.models.load_model("data/ModeloEntrenado.h5", compile=False)
+    return modelo
+
+def cargar_variables_texto():
+    # Cargar vocabulario
+    with open("data/vocabulario.txt", "r", encoding="utf-8") as f:
+        vocab = [line.strip() for line in f.readlines()]
+
+    # Reconstruir TextVectorization
+    text_vectorization = TextVectorization(
+        max_tokens=20000,
+        output_mode="int",
+        output_sequence_length=600,
+        vocabulary=vocab
+    )
+
+    # Cargar etiquetas
+    with open("data/etiquetas.txt", "r", encoding="utf-8") as f:
+        etiquetas = [line.strip() for line in f.readlines()]
+
+    return vocab, text_vectorization, etiquetas
+
+# * Configuración de los estados de sesión
 if "page_icon" not in st.session_state:
     st.session_state.page_icon = "📚"
     
@@ -15,7 +41,11 @@ if "rnn" not in st.session_state:
     
 if "transformers" not in st.session_state:
     st.session_state.transformers = False
+    
+if "user_input" not in st.session_state:
+    st.session_state.user_input = ""
 
+# * Configuración de la página
 st.set_page_config(
     page_title="Clasificador de Emociones", 
     layout="centered", 
@@ -40,9 +70,32 @@ cols = st.columns(2)
 if cols[0].button("RNN", use_container_width=True):
     st.session_state.rnn = True
     st.session_state.transformers = False
-    st.switch_page("pages/1_📚_RNN.py")
-    
+
+# Botón para Transformers
 if cols[1].button("Transformers", use_container_width=True):
     st.session_state.rnn = False
     st.session_state.transformers = True
-    st.switch_page("pages/2_📚_Transformers.py")
+
+# Mostrar input solo si se ha seleccionado un modelo
+if st.session_state.rnn or st.session_state_transformers:
+    user_input = st.text_input("✍️ Escriba la frase que gustes (en inglés):")
+
+    if st.button("🔍 Predecir emoción") and user_input:
+        st.session_state.user_input = user_input
+
+        if st.session_state.rnn:
+            st.switch_page("pages/1_📚_RNN.py")
+        elif st.session_state.transformers:
+            st.switch_page("pages/2_📚_Transformers.py")
+
+
+    # texto = tf.convert_to_tensor([[user_input]])
+    # vectorizado = text_vectorization(texto)
+    # prediccion = modelo(vectorizado).numpy()[0]
+
+    # # Mostrar emoción más probable
+    # indice_pred = np.argmax(prediccion)
+    # emocion = etiquetas[indice_pred]
+    # confianza = prediccion[indice_pred]
+
+    # st.success(f"**Emoción detectada:** {emocion} ({confianza:.2%})")
--- src/pages/1_📚_RNN.py
+++ src/pages/1_📚_RNN.py
@@ -4,7 +4,13 @@ st.header("🧠 Modelos de Redes Neuronales Recurrentes actuando")
 
 st.divider()
 
-user_input = st.text_input("✍️ Escriba la frase que gustes (en inglés): ")
+user_input_rnn = st.text_input("✍️ Escriba la frase que gustes (en inglés): ")
+if user_input_rnn:
+    with st.chat_message("user", avatar=":material/emoji_language:"):
+        st.write(user_input_rnn)
+    st.session_state.user_input = user_input_rnn
+
+user_input = st.session_state.get("user_input", "")
 if user_input:
     with st.chat_message("user", avatar=":material/emoji_language:"):
         st.write(user_input)
