573288fe450f4ab899897d9041be30d5f624eb02
Juandiego001 <juandiego14012003@gmail.com>
2025-05-31 23:25:33 -0500

    fix/transformers-and-summarize

--- src/pages/3_📚_Summarize_Transformers.py
+++ src/pages/3_📚_Summarize_Transformers.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from transformers_model import create_summary
 
 st.header("🧠 Modelos Transformers actuando")
 
@@ -8,3 +9,5 @@ user_input = st.text_input("✍️ Escriba la frase que gustes (en inglés): ")
 if user_input:
     with st.chat_message("user", avatar=":material/emoji_language:"):
         st.write(user_input)        
+        summary = 'Summary.\n' + create_summary(user_input)
+        st.write(summary)
